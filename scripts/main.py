8baab488c5ff4ab135489493e88cd40eeb1ff31a
rithikkulkarni <rithik.r.kulkarni@gmail.com>
2025-07-12 20:41:20 -0400

    Removed channel ids for generic codebase

--- scripts/main.py
+++ scripts/main.py
@@ -49,45 +49,7 @@ def get_all_video_ids_from_channel(channel_id):
 
 if __name__ == "__main__":
     channel_ids = [
-    "UCsaGKqPZnGp_7N80hcHySGQ",
-    "UChBEbMKI1eCcejTtmI32UEw",
-    "UCTvYEid8tmg0jqGPDkehc_Q",
-    "UCpprBWvibvmOlI8yJOEAAjA",
-    "UC4rqhyiTs7XyuODcECvuiiQ",
-    "UCj-wTLj0p0YvTkS-9-ACm0A",
-    "UCR9Gcq0CMm6YgTzsDxAxjOQ",
-    "UC4Ndz98NI_-9VQM3E7fctnQ",
-    "UCckPYr9b_iVucz8ID1Q67sw",
-    "UCqSeA-rZs6GOfZrs9jZRXtA",
-    "UCoxcjq-8xIDTYp3uz647V5A",
-    "UC2C_jShtL725hvbm1arSV9w",
-    "UCUQo7nzH1sXVpzL92VesANw",
-    "UCRix1GJvSBNDpEFY561eSzw",
-    "UCc_-hy0u9-oKlNdMKHBudcQ",
-    "UCSOpcUkE-is7u7c4AkLgqTw",
-    "UC5I2hjZYiW9gZPVkvzM8_Cw",
-    "UCVYamHliCI9rw1tHR1xbkfw",
-    "UC9fSZHEh6XsRpX-xJc6lT3A",
-    "UCzJIliq68IHSn-Kwgjeg2AQ",
-    "UC8v4vz_n2rys6Yxpj8LuOBA",
-    "UC0KiGuCTrehqzSQ7ikjxSQw",
-    "UCnQhwPVwcP-DnbUZtIMrupw",
-    "UC0Ize0RLIbGdH5x4wI45G-A",
-    "UCXulruMI7BHj3kGyosNa0jA",
-    "UCVrvnobbNGGMsS5n2mJwfOg",
-    "UCqjwF8rxRsotnojGl4gM0Zw",
-    "UCzGLDaTu81nJDtWK10MniGg",
-    "UCplkk3J5wrEl0TNrthHjq4Q",
-    "UCWrtsravWX0ANhHiJXNlyXw",
-    "UCUKi4zY5ETSqrKAjTBgjM-g",
-    "UCpIafFPGutTAKOBHMtGen7g",
-    "UCzGEGjOCbgv9z9SF71QyI7g",
-    "UCE6acMV3m35znLcf0JGNn7Q",
-    "UC4v2tQ8GqP0RbmAzhp4IFkQ",
-    "UCSbyncU597LMwb3HhnAI_4w",
-    "UCMfXv2enRXepxG92VoxfrEg",
-    "UC4-CH0epzZpD_ARhxCx6LaQ",
-    "UCOpcACMWblDls9Z6GERVi1A"
+    # Channel ids here
 ]
 
 
